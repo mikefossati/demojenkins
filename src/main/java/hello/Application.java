d96b38f822d5b8343d9e39ca8f964aefd7412f27
cl237355571 <cl237355571@cl237355571.synapsisit.syngbl.int>
2017-01-26 13:56:20 -0300

    First commit

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,19 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Ejemplo
+ * 
+ * @author cl237355571
+ *
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
